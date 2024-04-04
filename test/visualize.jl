846046cdb2e3a5a585b31d16c152af0e440723a5
GiggleLiu <cacate0129@gmail.com>
2024-04-05 00:40:17 +0800

    fix test

--- test/visualize.jl
+++ test/visualize.jl
@@ -3,5 +3,7 @@ using LuxorGraphPlot.Luxor
 
 @testset "visualize" begin
     mps = SimpleTDVP.rand_mps([1,2,4,6,6,4,2,1])
-    display(mps)
+    @test SimpleTDVP.default_draw(mps) isa Luxor.Drawing
+    mpo = SimpleTDVP.rand_mpo([1,4,6,4,1])
+    @test SimpleTDVP.default_draw(mpo) isa Luxor.Drawing
 end
